2966b07f9b7ae76f57b227fd7ad5c6960ace3c1e
citrariskyseptiana <60418013+citrariskyseptiana@users.noreply.github.com>
2020-03-18 19:36:20 +0700

    Add files via upload

---
+++ database/migrations/2020_03_17_142553_createtablepemasukan.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Createtablepemasukan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pemasukan', function (Blueprint $table) {
+            $table->string('pemasukan_id',40);
+            $table->string('sumber-pemasukan');
+            $table->integer('nominal');
+            $table->datetime('tanggal');
+            $table->text('keterangan');
+
+            $table->primary('pemasukan_id');
+
+            //Relasi ke table sumber_pemasukan
+            $table->foreign('sumber-pemasukan')
+            ->references('id')->on('sumber')
+            ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pemasukan', function (Blueprint $table) {
+            //
+        });
+    }
+}
